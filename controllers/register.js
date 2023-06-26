7cd66d870f93bbb3faea73e5059765a594e3da79
Sidharth-Prabhakaran <sidharthp1994@gmail.com>
2023-06-26 16:43:13 -0700

    register issue solved

--- controllers/register.js
+++ controllers/register.js
@@ -1,3 +1,11 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+  //   port     : process.env.RDS_PORT,
+    database : process.env.RDS_DB_NAME
+  });
 const bcrypt = require('bcrypt');
 async function registerPostFunc(req, res) {
     try{
